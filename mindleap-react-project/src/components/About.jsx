0b75708d6afad21fc50e8424715e44bfd3bfb516
Vincent Vincent <toxicgeek8@gmail.com>
2024-08-18 14:02:47 +1000

    add about page

--- mindleap-react-project/src/components/About.jsx
+++ mindleap-react-project/src/components/About.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react'
+import thumbnailImg from '../assets/video-thumbnail.webp'
+// React Icons here
+import { IoPlayOutline } from "react-icons/io5";
+import { IoIosClose } from "react-icons/io";
+import { IoArrowForwardCircleSharp } from "react-icons/io5";
 
 const About = () => {
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
@@ -17,13 +22,45 @@ const About = () => {
         <div className='py-12 px-4 md:w-4/5 mx-auto flex flex-col md:flex-row items-center gap-8'>
           <div className='md:w-1/2 w-full mb-8 md:mb-0'>
             {/* Left Side */}
-            
+            {
+              !isVideoPlaying && (
+                <div className='relative'>
+                  <img src={thumbnailImg} alt="Thumbnail Image" className='w-full md:h-[446px] h-auto rounded-lg object-cover' />
+                  <button className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-primary p-3 rounded-full shadow-lg cursor-pointer' onClick={handleVideoPlay}>
+                    <IoPlayOutline className='text-white size-12 pl-1' />
+                  </button>
+                </div>
+              )
+            }
           </div>
           <div className='md:w-1/2 w-full'>
             {/* Right Side */}
-            Right Side
+            <h2 className='text-4xl font-secondary font-bold mb-4 leading-snug capitalize'>
+              Individual consult and therapy
+            </h2>
+            <p className='text-lg mb-12 md:pr-8'>
+              Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia. Lorem Ipsum is simply dummy text.
+            </p>
+            <button className='bg-primary text-white py-3.5 px-8 font-medium rounded-md hover:bg-primary/90'>
+              <a href="#contact" className='flex gap-1 items-center'>
+                <span>Get Started</span>
+                <IoArrowForwardCircleSharp />
+              </a>
+            </button>
           </div>
         </div>
+        {
+          isVideoPlaying && (
+            <div className='fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'>
+              <div className='relative w-full h-full flex items-center justify-center z-50'>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/9x7zAxxPRxU?si=xNMZtU6UacI2X4d3&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <button onClick={handleCloseVideo} className='absolute top-4 right-4 text-white text-2xl cursor-pointer'>
+                  <IoIosClose />
+                </button>
+              </div>
+            </div>
+          )
+        }
       </div>
     </div>
   )
