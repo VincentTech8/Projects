e07037b6ebc5783e90d5269a6a269c01df51feec
Vincent Vincent <toxicgeek8@gmail.com>
2024-08-24 12:13:31 +1000

    Add footer

--- mindleap-react-project/src/App.jsx
+++ mindleap-react-project/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import About from './components/About'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Pricing from './components/Pricing'
@@ -20,6 +21,7 @@ function App() {
       <Pricing />
       <Testimonial />
       <Contact />
+      <Footer />
     </div>
   )
 }
