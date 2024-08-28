26e0967405faf2fc57bbd4e302e11c5941146ae8
Vincent Vincent <toxicgeek8@gmail.com>
2024-08-28 21:26:45 +1000

    Add

---
+++ vincent-portfolio/app/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className='text-white mb-4 font-extrabold text-4xl lg:text-6xl'>
+        Hello, I'm Vincent
+      </h1>
+    </section>
+  )
+}
+
+export default HeroSection
