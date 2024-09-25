1a081e2f2fc373e122367d68f4f28dc44bca65e8
Vincent Vincent <toxicgeek8@gmail.com>
2024-09-26 00:43:20 +1000

    Add

--- vincent-portfolio/app/componenets/HeroSection.jsx
+++ vincent-portfolio/app/componenets/HeroSection.jsx
@@ -7,23 +7,26 @@ const HeroSection = () => {
             <div className='grid grid-cols-1 sm:grid-cols-12'>
                 <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
                     <h1 className="text-white mb-4 text-3xl sm:text-4xl lg:text-5xl lg:leading-normal font-extrabold">
-                        Hello, I'm Vincent
+                        <span className='text-transparent bg-clip-text' style={{ backgroundImage: "linear-gradient(to right, #96EC98, #01EEFC)" }}>
+                            Hello, I'm{" "}
+                        </span>
+                        Vincent
                     </h1>
-                    <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
+                    <p className="text-[#C5C6C7] text-base sm:text-lg lg:text-xl mb-6">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
                         voluptuous.
                     </p>
                     <div>
-                        <button className='px-6 py-3 w-3/5 sm:w-fit rounded-full sm:mr-4 bg-white hover:bg-slate-200 text-black'>
+                        <button className='px-6 py-3 w-3/5 sm:w-fit rounded-full sm:mr-4  text-black hover:opacity-80' style={{ backgroundImage: "linear-gradient(to right, #96EC98, #01EEFC)" }}>
                             Hire me
                         </button>
-                        <button className='px-6 py-3 w-3/5 sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>
+                        <button className='px-6 py-3 w-3/5 sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border mt-3' style={{ borderColor: '#01EEFC' }}>
                             Download CV
                         </button>
                     </div>
                 </div>
                 <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
-                    <div className='rounded-full bg-[#323131] w-[175px] h-[175px] sm:w-[225px] sm:h-[225px] lg:w-[275px] lg:h-[275px] relative'>
+                    <div className='rounded-full bg-[#232323] w-[175px] h-[175px] sm:w-[225px] sm:h-[225px] lg:w-[275px] lg:h-[275px] relative'>
                         <Image
                             src="/images/character.png"
                             width={125}
--- vincent-portfolio/app/page.js
+++ vincent-portfolio/app/page.js
@@ -2,7 +2,7 @@ import HeroSection from "./componenets/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#000000]">
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
       </div>
