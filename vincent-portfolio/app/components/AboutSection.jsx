507fb5c16dadeb4d92ae142982bd0b3b440b2029
Vincent Vincent <toxicgeek8@gmail.com>
2024-09-27 14:30:31 +1000

    Add

--- vincent-portfolio/app/components/AboutSection.jsx
+++ vincent-portfolio/app/components/AboutSection.jsx
@@ -156,29 +156,39 @@ import {
 
 const AboutSection = () => {
   return (
-    <div className='min-h-[70vh] flex items-center justify-center py-12 xl:py-0'>
-      <div className="container mx-auto">
-        <Tabs defaultValue='about' className='flex flex-col xl:flex-row gap-[60px]'>
-          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
-            <TabsTrigger value="about">About me</TabsTrigger>
-            <TabsTrigger value="experience">Experience</TabsTrigger>
-            <TabsTrigger value="education">Education</TabsTrigger>
-            <TabsTrigger value="skills">Skills</TabsTrigger>
-          </TabsList>
-          {/* content */}
-          <div className='text-white min-h-[60vh] w-full'>
-            {/* about */}
-            <TabsContent value="about" className="w-full">
-              about
-            </TabsContent>
-            {/* experience */}
-            <TabsContent value="experience" className="w-full">
-              experience
-            </TabsContent>
-          </div>
-        </Tabs>
+    <section>
+      <div className='min-h-[70vh] flex items-center justify-center py-12 xl:py-0'>
+        <div className="container mx-auto">
+          <Tabs defaultValue='about' className='flex flex-col xl:flex-row gap-[60px]'>
+            <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+              <TabsTrigger value="about">About me</TabsTrigger>
+              <TabsTrigger value="experience">Experience</TabsTrigger>
+              <TabsTrigger value="education">Education</TabsTrigger>
+              <TabsTrigger value="skills">Skills</TabsTrigger>
+            </TabsList>
+            {/* content */}
+            <div className='text-white min-h-[60vh] w-full'>
+              {/* about */}
+              <TabsContent value="about" className="w-full">
+                about
+              </TabsContent>
+              {/* experience */}
+              <TabsContent value="experience" className="w-full">
+                experience
+              </TabsContent>
+              {/* education */}
+              <TabsContent value="education" className="w-full">
+                education
+              </TabsContent>
+              {/* skills */}
+              <TabsContent value="skills" className="w-full">
+                skills
+              </TabsContent>
+            </div>
+          </Tabs>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
--- vincent-portfolio/components/ui/tabs.jsx
+++ vincent-portfolio/components/ui/tabs.jsx
@@ -22,7 +22,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center w-full bg-[#27272c] justify-center whitespace-nowrap text-white rounded-lg p-3 text-base font-medium ring-offset-white transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-gradient-to-r data-[state=active]:from-[#96EC98] data-[state=active]:to-[#01EEFC] data-[state=active]:text-black data-[state=active]:font-bold data-[state=active]:shadow-sm",
+      "inline-flex items-center w-full bg-[#27272c] justify-center whitespace-nowrap text-white rounded-lg p-3 text-base font-medium ring-offset-white transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-gradient-to-r data-[state=active]:from-[#96EC98] data-[state=active]:to-[#01EEFC] data-[state=active]:text-black data-[state=active]:font-bold data-[state=active]:shadow-sm data-[state=inactive]:hover:opacity-80",
       className
     )}
     {...props} />
