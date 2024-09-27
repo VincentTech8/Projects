631279e0fb177c670a13df684bca65c3e3c33367
Vincent Vincent <toxicgeek8@gmail.com>
2024-09-27 11:06:45 +1000

    Add

---
+++ vincent-portfolio/app/components/AboutSection.jsx
@@ -0,0 +1,18 @@
+"use client"
+import React from 'react'
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaFigma,
+  FaReact,
+  FaNodeJs
+} from 'react-icons/fa'
+
+const AboutSection = () => {
+  return (
+    <div>AboutSection</div>
+  )
+}
+
+export default AboutSection
--- vincent-portfolio/app/page.js
+++ vincent-portfolio/app/page.js
@@ -1,10 +1,14 @@
-import HeroSection from "./componenets/HeroSection";
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#000000]">
+    <main className="flex flex-col min-h-screen bg-[#000000]">
+      <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
